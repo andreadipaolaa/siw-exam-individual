ea42978d46dd6a769f32308f253904a96cc91815
andreadipaola <andrea.dipaola@outlook.com>
2022-05-30 18:31:44 +0200

    creato progetto su intelliJ

---
+++ siw-exam-individual/src/main/java/com/example/siwexamindividual/SiwExamIndividualApplication.java
@@ -0,0 +1,13 @@
+package com.example.siwexamindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwExamIndividualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiwExamIndividualApplication.class, args);
+    }
+
+}
---
+++ siw-exam-individual/src/test/java/com/example/siwexamindividual/SiwExamIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siwexamindividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwExamIndividualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
